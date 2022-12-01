046e92c8594bb6bbd74231140aee467c45c71f53
Ingerid Dale <ingerid.dale@nb.no>
2022-12-01 15:06:56 +0100

    doc: add docstring explaining the main function

--- sentiment.py
+++ sentiment.py
@@ -2,32 +2,42 @@ import pandas as pd
 import dhlab as dh
 
 
+# Helper functions
+def _count_sentiment_tokens(coll, terms):
+    target_terms = terms.join(coll, how="inner", on="terms")
+    return target_terms
+
+def _group_index_terms(df):
+    df.index = df.index.str.lower()
+    df.index.name = "index"
+    df = df.groupby('index')["counts"].sum().to_frame("counts")
+    assert df.index.is_unique, "There are duplicate collocation terms after case-insensitive transformation."
+    return df
 
 
 def coll_sentiment(coll, word="barnevern", return_score_only=False):
-    
+    """Compute a sentiment score of positive and negative terms in `coll`.
+
+    The collocations of the `word` are used to count occurrences of positive and negative terms.
+
+    :param coll: a collocations dataframe or a dh.Corpus where `word` occurs.
+    :param str word: a word to estimate sentiment scores for
+    :param bool return_score_only: If True,
+        return a tuple with the absolute counts for positive and negative terms.
+    """
     if isinstance(coll, dh.Corpus):
         coll = coll.coll(word).frame
-        
-        
-    # Data import 
-    pos = pd.read_csv("norsentlex/Fullform/Fullform_Positive_lexicon.txt", names=["terms"])
-    neg = pd.read_csv("norsentlex/Fullform/Fullform_Negative_lexicon.txt", names=["terms"])
-    
-    
-    def _count_sentiment_tokens(coll, terms):  
-        target_terms = terms.join(coll, how="inner", on="terms")
-        return target_terms    
-    
-    coll.index = coll.index.str.lower()
-    coll.index.name = "index"
-    coll = coll.groupby('index')["counts"].sum().to_frame("counts")
-    
-    assert coll.index.is_unique, "Collocation index is not unique..."
+
+    coll = _group_index_terms(coll)
+
+    # Data import
+    data_dir = "../norsentlex/Fullform/"
+    pos = pd.read_csv(data_dir + "/Fullform_Positive_lexicon.txt", names=["terms"])
+    neg = pd.read_csv(data_dir + "/Fullform_Negative_lexicon.txt", names=["terms"])
 
     positive_counts = _count_sentiment_tokens(coll, pos)
     negative_counts = _count_sentiment_tokens(coll, neg)
-    
+
     if return_score_only:
         return positive_counts.counts.sum(), negative_counts.counts.sum()
 
@@ -42,27 +52,27 @@ def coll_sentiment(coll, word="barnevern", return_score_only=False):
 
     positive_counts["sentiment"] = "pos"
     negative_counts["sentiment"] = "neg"
-    neutral_counts["sentiment"] = "neutral" 
-    
+    neutral_counts["sentiment"] = "neutral"
+
     return pd.concat([positive_counts, negative_counts, neutral_counts])
 
 
 def sentiment_by_place(cities=["Kristiansand", "Stavanger"], from_year=1999, to_year=2010):
 
 
-    
+
     for city in cities:
         lst = []
         for year in range(from_year, to_year):
             corpus = dh.Corpus(doctype="digavis", freetext=f"city: {city} year: {year}", limit=1000)
             pos, neg = coll_sentiment(corpus, "barnevern", return_score_only=True)
-            
+
             lst.append(
                 pd.DataFrame(
-                    [[pos, neg, pos-neg]], 
-                    index=[year], 
+                    [[pos, neg, pos-neg]],
+                    index=[year],
                     columns=["positive", "negative", "sum"]
                     )
                 )
-        
-        yield pd.concat(lst)   
+
+        yield pd.concat(lst)
