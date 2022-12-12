eff65b351099dd63ae792ceea243ae3c79c0cb48
Ingerid Dale <ingerid.dale@nb.no>
2022-12-12 16:34:07 +0100

    feat: add function to score sentiments
    Signed-off-by: Ingerid Dale <ingerid.dale@nb.no>

--- sentiment.py
+++ sentiment.py
@@ -1,14 +1,22 @@
+from pathlib import Path
+from typing import Generator
 import pandas as pd
+import numpy as np
+
 import dhlab as dh
+from dhlab.api.dhlab_api import urn_collocation, get_document_frequencies, collocation, word_variant, word_paradigm
+from dhlab.text.utils import urnlist
 
 
 # Helper functions
-def count_sentiment_tokens(coll, terms):
+def count_sentiment_tokens(coll: pd.DataFrame, terms: pd.Series) -> pd.DataFrame:
+    """Combine collocation counts with a series of terms (positive or negative)."""
     target_terms = terms.join(coll, how="inner", on="terms")
     return target_terms
 
 
-def group_index_terms(df):
+def group_index_terms(df: pd.DataFrame) -> pd.DataFrame:
+    """Group duplicate index terms, make them case-insensitive, and sum up their frequency counts."""
     df.index = df.index.str.lower()
     df.index.name = "index"
     df = df.groupby('index')["counts"].sum().to_frame("counts")
@@ -16,12 +24,11 @@ def group_index_terms(df):
     return df
 
 
-def make_list(value):
+def make_list(value) -> list:
     """Turn a string or list into a list.
 
     :param value: Can be a list, a single valued string, a comma-separated string of values,
         or a multiline string of values separated by newline.
-
     """
     if isinstance(value, str):
         if value.__contains__("\n"):
@@ -35,9 +42,30 @@ def make_list(value):
         assert isinstance(value, list)
         return value
 
+def load_corpus_from_file(file_path):
+    """Load a Corpus object from an excel or csv file."""
+    try:
+        corpus = dh.Corpus.from_df(pd.read_excel(file_path)) if file_path.endswith(".xlsx") else dh.Corpus.from_csv(file_path)
+    except FileNotFoundError:
+        print("The corpus file must be a .csv or .xlsx file: ", file_path)
+        corpus = dh.Corpus()
+    return corpus
 
 
-def timestamp_generator(from_year, to_year):
+def load_sentiment_terms(fpath: str = None, sentiment="Positive") -> pd.Series:
+    """Load a sentiment lexicon from file.
+
+     By defalt, use the Positive sentiment lexicon from [``ǸorsentLex``](https://github.com/ltgoslo/norsentlex).
+
+     - [Lexicon information in neural sentiment analysis:
+     a multi-task learning approach](https://aclanthology.org/W19-6119) (Barnes et al., NoDaLiDa 2019)
+    """
+    if fpath is None or not Path(fpath).exists:
+        fpath =  f"https://raw.githubusercontent.com/ltgoslo/norsentlex/master/Fullform/Fullform_{sentiment}_lexicon.txt"
+    return pd.read_csv(fpath, names=["terms"])
+
+
+def timestamp_generator(from_year: int, to_year: int) -> Generator:
     """Generate a timestamp per day in the period ``from_year``-``to_year``."""
     # range of timestamps
     timestamp_range = pd.date_range(start=f"{from_year}-01-01", end=f"{to_year}-12-31")
@@ -47,14 +75,15 @@ def timestamp_generator(from_year, to_year):
         yield date
 
 
-def read_spreadsheet(file_path):
-    return pd.read_excel(file_path) if file_path.endswith(".xlsx") else pd.read_csv(file_path)
+def strip_empty_cols(dhobj):
+    """Remove columns without values from a DhlabObj."""
+    return dhobj.frame.dropna(axis=1, how="all").fillna("")
 
 
 def coll_sentiment(coll, word="barnevern", return_score_only=False):
     """Compute a sentiment score of positive and negative terms in `coll`.
 
-    The collocations of the `word` are used to count occurrences of positive and negative terms.
+    The collocations of the ``word`` are used to count occurrences of positive and negative terms.
 
     :param coll: a collocations dataframe or a dh.Corpus where `word` occurs.
     :param str word: a word to estimate sentiment scores for
@@ -67,9 +96,8 @@ def coll_sentiment(coll, word="barnevern", return_score_only=False):
     coll = group_index_terms(coll)
 
     # Data import
-    data_dir = "../norsentlex/Fullform/"
-    pos = pd.read_csv(data_dir + "/Fullform_Positive_lexicon.txt", names=["terms"])
-    neg = pd.read_csv(data_dir + "/Fullform_Negative_lexicon.txt", names=["terms"])
+    pos = load_sentiment_terms(sentiment="Positive")
+    neg = load_sentiment_terms(sentiment="Negative")
 
     positive_counts = count_sentiment_tokens(coll, pos)
     negative_counts = count_sentiment_tokens(coll, neg)
@@ -111,3 +139,64 @@ def sentiment_by_place(keyword:str ="barnevern", cities=["Kristiansand", "Stavan
                 )
 
         yield pd.concat(lst)
+
+
+def fetch_finegrained_collocations(urn: str, word: str, before: int, after: int) -> pd.DataFrame:
+    """For a given URN and a given word, fetch collocations with """
+
+    coll = urn_collocation(
+        urns=[urn],
+        word=word,
+        before=before,
+        after=after
+    )
+    coll = coll.loc[[x for x in coll.index if x.isalpha()]]
+    coll.index = [x.lower() for x in coll.index]
+    coll = coll.groupby(coll.index).sum()
+    return coll
+
+
+def score_sentiment(urn, word, before, after):
+    """Calculate a sentiment score for the contexts of ``word`` in a given publication (``URN``)."""
+    collocations = fetch_finegrained_collocations(urn, word, before, after)
+    pos, neg = coll_sentiment(collocations,return_score_only=True)
+    values = [urn, word, pos, neg, pos-neg]
+    names = ["urn", "target_word", "positive", "negative", "sentimentscore"]
+    return dict(zip(names, values))
+
+
+
+def unpivot(frame):
+    """Reshape a dataframe with multiple indexes.
+
+    Util function copied from Pandas docs:
+    https://pandas.pydata.org/pandas-docs/stable/user_guide/reshaping.html
+    """
+    N, K = frame.shape
+    data = {
+        "frequency": frame.to_numpy().ravel("F"),
+        "urn": np.asarray(frame.columns).repeat(N),
+        "word": np.tile(np.asarray(frame.index), K),
+    }
+
+    return pd.DataFrame(data, columns=["word", "urn", "frequency"])
+
+
+
+def score_sentiment(urn, word, before, after):
+    """Calculate a sentiment score for the contexts of ``word`` in a given publication."""
+    collocations = fetch_finegrained_collocations(urn, word, before, after)
+    pos, neg = coll_sentiment(collocations, word, return_score_only=True)
+    values = [pos, neg, pos-neg]
+    names = ["positive", "negative", "sentimentscore"]
+    return dict(zip(names, values))
+
+
+def count_and_score_target_word(corpus: dh.Corpus, search_terms: str):
+    words = make_list(search_terms)
+    counts = corpus.count(words)
+    term_counts = unpivot(counts.frame)
+    scored_terms = term_counts.apply(lambda x: score_sentiment(x.urn, x.word, before, after ), axis=1, result_type="expand")
+    term_counts.loc[:, ["positive", "negative", "sentimentscore"]] = scored_terms
+    df = strip_empty_cols(corpus)
+    return df.merge(term_counts, how="inner", left_on="urn", right_on="urn")
